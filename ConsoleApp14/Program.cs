eba7a19d37b8ba97408f309c5174086852db056a
sibrava <sibrava@L02PCUCITEL>
2026-02-10 14:50:58 +0100

    xxx

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Foo bar");
             Console.WriteLine("Boo baz");
+            Console.WriteLine("xxx");
         }
     }
 }
